e686eaae966de3dc728a64d4c2c5747bceb7f20a
zorim <z.pavllova@gmail.com>
2024-08-08 14:10:26 +0300

    CloudWatch

--- .github/workflows/cloudwatch/cloudwatch_alarm.tf
+++ .github/workflows/cloudwatch/cloudwatch_alarm.tf
@@ -1,64 +1,48 @@
 
-resource "aws_cloudwatch_metric_alarm" "lambda_error_alarm" {
-  alarm_name          = "lambda_error_alarm"
-  comparison_operator = "GreaterThanOrEqualToThreshold"
-  evaluation_periods  = "1"
-  metric_name         = "Errors"
-  namespace           = "AWS/Lambda"
-  period              = "60"
-  statistic           = "Sum"
-  threshold           = "1"
-  alarm_description   = "Alarm when Lambda function errors exceed 1"
-  dimensions = {
-    FunctionName = aws_lambda_function.lambda_function.function_name
-  }
-}
-
-
-# resource "aws_cloudwatch_alarm" "lambda_error_alarm" {
+# resource "aws_cloudwatch_metric_alarm" "lambda_error_alarm" {
 #   alarm_name          = "lambda_error_alarm"
 #   comparison_operator = "GreaterThanOrEqualToThreshold"
 #   evaluation_periods  = "1"
-#   metric_name         = aws_cloudwatch_metric_filter.lambda_metric_filter.metric_transformation[0].name
-#   namespace           = aws_cloudwatch_metric_filter.lambda_metric_filter.metric_transformation[0].namespace
+#   metric_name         = "Errors"
+#   namespace           = "AWS/Lambda"
 #   period              = "60"
 #   statistic           = "Sum"
 #   threshold           = "1"
-
 #   alarm_description   = "Alarm when Lambda function errors exceed 1"
-#   actions_enabled     = true
+#   dimensions = {
+#     FunctionName = aws_lambda_function.lambda_function.function_name
+#   }
 # }
 
 
+# resource "aws_cloudwatch_log_group" "function_log_group" {
+#   name              = "/aws/lambda/${aws_lambda_function.lambda_function.function_name}"
+#   retention_in_days = 7
+#   lifecycle {
+#     prevent_destroy = false
+#   }
+# }
 
-resource "aws_cloudwatch_log_group" "function_log_group" {
-  name              = "/aws/lambda/${aws_lambda_function.lambda_function.function_name}"
-  retention_in_days = 7
-  lifecycle {
-    prevent_destroy = false
-  }
-}
+# resource "aws_cloudwatch_log_group" "lambda_log_group" {
+#   name              = "/aws/lambda/${aws_lambda_function.lambda_function.function_name}"
+#   retention_in_days = 7
+# }
 
-resource "aws_cloudwatch_log_group" "lambda_log_group" {
-  name              = "/aws/lambda/${aws_lambda_function.lambda_function.function_name}"
-  retention_in_days = 7
-}
+# resource "aws_cloudwatch_log_stream" "lambda_log_stream" {
+#   name           = "lambda_log_stream"
+#   log_group_name = aws_cloudwatch_log_group.function_log_group.name
+# }
 
-resource "aws_cloudwatch_log_stream" "lambda_log_stream" {
-  name           = "lambda_log_stream"
-  log_group_name = aws_cloudwatch_log_group.function_log_group.name
-}
+# resource "aws_cloudwatch_log_metric_filter" "lambda_metric_filter" {
+#   name           = "lambda_metric_filter"
+#   log_group_name = aws_cloudwatch_log_group.function_log_group.name
+#   pattern        = "ERROR"
 
-resource "aws_cloudwatch_log_metric_filter" "lambda_metric_filter" {
-  name           = "lambda_metric_filter"
-  log_group_name = aws_cloudwatch_log_group.function_log_group.name
-  pattern        = "ERROR"
-
-  metric_transformation {
-    name      = "LambdaErrors"
-    namespace = "LambdaMetrics"
-    value     = "1"
-  }
-}
+#   metric_transformation {
+#     name      = "LambdaErrors"
+#     namespace = "LambdaMetrics"
+#     value     = "1"
+#   }
+# }
 
 
--- .github/workflows/cloudwatch/main.tf
+++ .github/workflows/cloudwatch/main.tf
@@ -19,16 +19,58 @@ resource "aws_lambda_permission" "allow_cloudwatch_to_call_check_foo" {
   source_arn    = "${aws_cloudwatch_event_rule.every_one_minute.arn}"
 }
 
-resource "aws_cloudwatch_event_rule" "every_one_minute" {
-  name                = "every-one-minute"
-  description         = "Fires every one minutes"
-  schedule_expression = "rate(1 minute)"
+
+# S3 Bucket configure
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "my-bucket"
 }
 
-resource "aws_lambda_permission" "allow_cloudwatch_to_call_check_foo" {
-  statement_id  = "AllowExecutionFromCloudWatch"
-  action        = "lambda:InvokeFunction"
-  function_name = "${aws_lambda_function.lambda.function_name}"
-  principal     = "events.amazonaws.com"
-  source_arn    = "${aws_cloudwatch_event_rule.every_one_minute.arn}"
+resource "aws_s3_bucket_notification" "bucket_notification" {
+  bucket = aws_s3_bucket.my_bucket.id
+
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.my_lambda.arn
+    events              = ["s3:ObjectCreated:*"]
+  }
 }
+
+
+# SQS Bucket configure
+resource "aws_sqs_queue" "my_queue" {
+  name = "my-queue"
+}
+
+resource "aws_cloudwatch_metric_alarm" "sqs_alarm" {
+  alarm_name          = "sqs-alarm"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = "1"
+  metric_name         = "ApproximateNumberOfMessagesVisible"
+  namespace           = "AWS/SQS"
+  period              = "60"
+  statistic           = "Average"
+  threshold           = "10"
+  alarm_description   = "Alarm when there are more than 10 messages in the queue"
+  actions_enabled     = true
+  alarm_actions       = [aws_sns_topic.sqs_alarm_topic.arn]
+  dimensions = {
+    QueueName = aws_sqs_queue.my_queue.name
+  }
+}
+#lambda function matric
+resource "aws_cloudwatch_metric_alarm" "lambda_alarm" {
+  alarm_name          = "lambda-alarm"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = "1"
+  metric_name         = "Errors"
+  namespace           = "AWS/Lambda"
+  period              = "60"
+  statistic           = "Sum"
+  threshold           = "1"
+  alarm_description   = "Alarm when the Lambda function errors"
+  actions_enabled     = true
+  alarm_actions       = [aws_sns_topic.lambda_alarm_topic.arn]
+  dimensions = {
+    FunctionName = aws_lambda_function.my_lambda.function_name
+  }
+}
+

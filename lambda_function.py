14d6dd18b935c41b42454751260b70573afa06cf
zorim <z.pavllova@gmail.com>
2024-08-12 12:59:04 +0300

    SNS_update

--- lambda_function.py
+++ lambda_function.py
@@ -14,9 +14,14 @@ def lambda_handler(event, context):
     if 'Records' in event:
         for record in event['Records']:
             try:
-                # Read message from SQS
-                message = record['body']
-                logger.info(f"Message from SQS: {message}")
+                # Check if the event is from SNS
+                if 'SNS' in record:
+                    message = record['Sns']['Message']
+                    logger.info(f"Message from SNS: {message}")
+                else:
+                    # Read message from SQS
+                    message = record['body']
+                    logger.info(f"Message from SQS: {message}")
 
                 # Generate a unique ID for the file
                 file_key = "message.txt"
@@ -45,3 +50,96 @@ def lambda_handler(event, context):
             'statusCode': 400,
             'body': json.dumps('No Records found in event')
         }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# import json
+# import boto3
+# import logging
+
+# s3 = boto3.client('s3')
+# bucket_name = 'dreamsofcode-noticeably-constantly-wondrous-wolf'
+
+# logger = logging.getLogger()
+# logger.setLevel(logging.INFO)
+
+# def lambda_handler(event, context):
+#     logger.info("Received event: " + json.dumps(event, indent=2))
+
+#     if 'Records' in event:
+#         for record in event['Records']:
+#             try:
+#                 # Read message from SQS
+#                 message = record['body']
+#                 logger.info(f"Message from SQS: {message}")
+
+#                 # Generate a unique ID for the file
+#                 file_key = "message.txt"
+#                 logger.info(f"Generated file key: {file_key}")
+
+#                 # Upload a file to S3 with the unique ID as the key
+#                 logger.info("Attempting to upload file to S3")
+#                 s3.put_object(Bucket=bucket_name, Key=file_key, Body=message)
+#                 logger.info(f"File uploaded successfully with key: {file_key}")
+
+#             except Exception as e:
+#                 logger.error(f"Error processing record: {e}")
+#                 raise e  # Ensure the error is raised to trigger retries/DLQ
+
+#         return {
+#             'statusCode': 200,
+#             'body': json.dumps({
+#                 'message': 'Messages processed successfully',
+#                 'sqs_message': message,
+#                 'file_key': file_key,
+#                 's3': 'Hello from S3'
+#             })
+#         }
+#     else:
+#         return {
+#             'statusCode': 400,
+#             'body': json.dumps('No Records found in event')
+#         }
+
+
+
